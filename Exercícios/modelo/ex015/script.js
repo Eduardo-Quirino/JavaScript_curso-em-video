2b9971da0cde3c190a31328a3b598cc14c1f1e0a
Eduardo Quirino <edu.3532@gmail.com>
2021-02-03 10:02:39 -0300

    Terminado ex015, corrigido erro e alterado pasta img

--- Exercícios/modelo/ex015/script.js
+++ Exercícios/modelo/ex015/script.js
@@ -1,53 +1,51 @@
 function verificar() {
     var data = new Date()
     var ano = data.getFullYear()
-    var fano = document.getElementById('txtano')
+    var fano = document.querySelector('input#txtano')
     var res = document.querySelector('div#res')
+
     if (fano.value.length == 0 || Number(fano.value) > ano) {
-        window.alert('[ERRO] Verifique seus dados e tente novamente!')
+        window.alert('[ERRO] Verifique os dados e tente novamente! ')
     } else {
-        var fsex = document.getElementsByTagName('radsex')
+        var fsex = document.getElementsByName('radsex')
         var idade = ano - Number(fano.value)
         var gênero = ''
         var img = document.createElement('img')
-
         img.setAttribute('id', 'foto')
-
-        //tirar as aspas colocado por conta temp aula ex3 modulo 14:40
-        if ('fsex[0].checked') {
-            gênero = 'Homem'
+        if (fsex[0].checked) {
+            gênero = `Homem`
             if (idade >= 0 && idade < 10) {
-                //imagem criança
-                img.setAttribute('src', 'img/crianças/menino 250x250.png')
+                //criança
+                img.setAttribute('src', 'img/bebe/bebe menino.png')
             } else if (idade < 21) {
                 //jovem
                 img.setAttribute('src', 'img/jovens/jovem menino 250x250.png')
 
             } else if (idade < 50) {
-                //imagem adulto
+                //adulto
                 img.setAttribute('src', 'img/adultos/adulto 250x250.png')
 
             } else {
-                //imagem idoso
-                img.setAttribute('src', 'img/idosos/idoso 250x250.png ')
-            }
-        } else if ('fsex[1].checked') {
-            gênero = 'Mulher'
-            if (idade >= 0 && idade < 10) {
-                //imagem criança
-                img.setAttribute('src', 'img/crianças/menina 250x250.png ')
+                //idoso
+                img.setAttribute('src', 'img/idosos/idoso 250x250.png')
 
+            }
+        } else if (fsex[1].checked) {
+            gênero = `Mulher`
+            if (idade >= 0 && idade < 10) {
+                //criança
+                img.setAttribute('src', 'img/bebe/bebe menina.png')
             } else if (idade < 21) {
-                //imagem jovem
-                img.setAttribute('src', 'img/jovens/jovem menina 250x250.png ')
+                //jovem
+                img.setAttribute('src', 'img/jovens/jovem menina 250x250.png')
 
             } else if (idade < 50) {
-                //imagem adulta
-                img.setAttribute('src', 'img/adultos/adulta 250x250.png ')
+                //adulto
+                img.setAttribute('src', 'img/adultos/adulta 250x250.png')
 
             } else {
-                //imagem idosa
-                img.setAttribute('src', 'img/idosos/idosa 250x250.png ')
+                //idoso
+                img.setAttribute('src', 'img/idosos/idosa 250x250.png')
 
             }
         }
